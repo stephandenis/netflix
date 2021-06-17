fe45df32c99cddea793a96f11c3cfe2fc9c4b02c
Stephan <stephangdenis@gmail.com>
2021-06-17 08:56:57 -0300

    newComponent[Row] - maps the api object to render the movies

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,6 +1,8 @@
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Navbar from "./Navbar";
+import requests from "./Requests";
+import Row from "./Row";
 
 const HomeScreen = () => {
   return (
@@ -10,6 +12,18 @@ const HomeScreen = () => {
       {/* Banner */}
       <Banner />
       {/* Rows */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
