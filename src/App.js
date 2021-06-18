1b49f26d0b6d02f00f08dad8d66e2c5911c46022
Stephan <stephangdenis@gmail.com>
2021-06-18 06:48:25 -0300

    newComponent[LoginScreen + SignUpScreen]

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import HomeScreen from "./screens/HomeScreen";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
       </Router>
     </div>
   );
